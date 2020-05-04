9f0e03a8a8a247bdd52f4df142dca8ad2a875b2f
NYuya <noooochan5@gmail.com>
2020-05-04 14:50:51 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,51 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'bookmarks#top'
+
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations',
+    passwords: 'customers/passwords'
+  }
+
+  namespace :customers do
+    get 'confirm' => 'customers#confirm'
+    patch 'hide' => 'customers#hide'
+    put 'hide' => 'customers#hide'
+  end
+
+  scope module: :customers do
+      resources :customers, only: [:show, :edit, :update]
+  end
+
+  get 'followed/:id' => 'relationships#followed', as: 'followed'
+  get 'follower/:id' => 'relationships#follower', as: 'follower'
+  resources :relationships, only: [:create, :destroy]
+
+
+  resources :bookmarks do
+    resource :favorites, only: [:create, :destroy]
+    resource :bookmark_comments, only: [:index, :edit, :destroy, :update, :create ]
+    resource :genres, only: [:create, :update, :destroy]
+  end
+
+  
+  resources :folders, only: [:create,:destroy,:update]
+  resources :folder_bookmarks, only: [:create,:destroy,:update]
+
+
+  devise_for :admins, controllers: {
+          sessions: 'admins/sessions'
+        }
+    namespace :admins do
+        get '/' => 'admins#top'
+        resources :bookmarks, only: [:show,:index,:edit, :update]
+        resources :genres, only: [:index,:create, :edit, :update]
+        resources :customers, only: [:show,:index,:edit, :update]
+        resources :bookmark_comments, only: [:index, :show, :update, :destroy]
+    end
+
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
