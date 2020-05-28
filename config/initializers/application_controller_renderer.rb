f551bb289f33818fd1da9c4f130465e2308196d1
NYuya <noooochan5@gmail.com>
2020-05-28 07:27:29 +0000

    Refile.secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '59c0dd47199ee966525fe7da9460ee0b2f80ff4427002fb02e91f51ed53579feb1d1772fd903baf29efd19b519b75a8eee2e9ac0c09cc18da063647fc343be9d'
