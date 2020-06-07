9bb0ce8aea150bfab44f3986eb1ea2333803f6fa
NYuya <noooochan5@gmail.com>
2020-06-07 08:59:48 +0000

    [Finish]bookmark_comment Error message

--- app/controllers/bookmark_comments_controller.rb
+++ app/controllers/bookmark_comments_controller.rb
@@ -10,12 +10,15 @@ class BookmarkCommentsController < ApplicationController
   end
 
   def create
-    bookmark = Bookmark.find(params[:bookmark_id])
+    @bookmark = Bookmark.find(params[:bookmark_id])
     @bookmark_comment = current_customer.bookmark_comments.new(bookmark_comment_params)
-    @bookmark_comment.bookmark_id = bookmark.id
+    @bookmark_comment.bookmark_id = @bookmark.id
     @bookmark_comment.score = Language.get_data(bookmark_comment_params[:bookmark_comment])
-    @bookmark_comment.save
+    if @bookmark_comment.save
     redirect_back(fallback_location: root_path)
+    else
+    render "bookmarks/show"
+    end
   end
 
   def destroy
--- app/models/bookmark_comment.rb
+++ app/models/bookmark_comment.rb
@@ -3,6 +3,6 @@ class BookmarkComment < ApplicationRecord
   belongs_to :customer
   belongs_to :bookmark
   validates :bookmark_comment, presence: true
-  validates :score, numericality:{ greater_than: -0.8 }
+  validates :score, numericality:{ greater_than: -0.9 }
 
 end
