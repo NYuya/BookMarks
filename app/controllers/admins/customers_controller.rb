7ab7c80c7fb4b565a364f4d92b200e10fcfb0926
NYuya <noooochan5@gmail.com>
2020-07-03 08:13:56 +0000

    admin customer show

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -3,15 +3,20 @@ class Admins::CustomersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-  	@customers = Customer.all
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @bookmarks = @customer.bookmarks
+    @folders = @customer.folders
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
   end
 end
